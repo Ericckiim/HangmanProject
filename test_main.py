1afe853c82a8aaa2a1507fc02f7a216026f928e0
Eric Kim <ekim09@students.cs.ubc.ca>
2025-11-09 20:32:00 -0800

    Added tests for checking character and space printing

--- test_main.py
+++ test_main.py
@@ -1,3 +1,13 @@
-from main import pick_word
-from main import check_charcter
+from main import check_character, space_printer, fill_answer
+
+
+def test_check_character():
+    assert check_character("d", "dinosaur") is True
+    assert check_character("d", "scream") is False
+
+def test_space_printer():
+    assert space_printer("hello") == "_ _ _ _ _ "
+    assert space_printer("h") == "_ "
+    assert space_printer("extremely") == "_ _ _ _ _ _ _ _ _ "
+
 
