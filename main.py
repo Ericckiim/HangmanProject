866f312fad6e9829b1b1e610f02a5e1000d43073
Eric Kim <ekim09@students.cs.ubc.ca>
2025-11-09 19:31:18 -0800

    Added word bank and outline for the functions needed

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hi")
+import random
+
+WORD_BANK = 
+ ['electricity', 'donkey', 'hardware', 'xerox', 'transistor', 'computer', 'desktop
+ 'engineering', 'hangman', 'circuit', 'imagination', 'robot', 'memory', 'power', 
+ 'submarine', 'chess', 'resistance', 'matrix', 'function', 'laser', 'mechanism', 
+ 'bodyguard', 'titanic', 'global', 'ozone', 'bridge', 'technology', 'spider', 'un
+ 'pyramid', 'sphere', 'member', 'warning', 'yourself', 'screen', 'language', 'ele
+ 'system', 'internet', 'parameter', 'traffic', 'network', 'filter', 'nucleus', 'r
+ 'automatic', 'microphone', 'cassette', 'operation', 'country', 'beautiful', 'mis
+ 'picture', 'teacher', 'superman', 'undertaker', 'alarm', 'process', 'keyboard', 
+ 'electron', 'certificate', 'grandfather', 'landmark', 'relativity', 'eraser', 'a
+ 'design', 'football', 'human', 'musician', 'egyptian', 'elephant', 'queen', 'rec
+ 'message', 'wallpaper', 'nationality', 'answer', 'wrong', 'statement', 'forest',
+ 'puzzle', 'voltage', 'current', 'mathematics', 'wisdom', 'dream', 'supermarket',
+ 'database', 'collection', 'barrier', 'project', 'sunlight', 'figure', 'graph', '
+ 'battle', 'hundred', 'signal', 'thousand', 'transformation', 'daughter', 'flower
+ 'communication', 'microwave', 'electronic', 'peace', 'wireless', 'delete', 'wind
+ 'brain', 'control', 'prophet', 'freedom', 'harbour', 'confidence', 'positive', '
+ 'harvest', 'hunger', 'woman', 'children', 'stranger', 'garden', 'pleasure', 'lau
+ 'between', 'recognition', 'tomorrow', 'autumn', 'monkey', 'spring', 'winter', 'm
+ 'classification', 'typewriter', 'success', 'difference', 'acoustics', 'astronomy
+ 'agreement', 'sorrow', 'christmas', 'silver', 'birthday', 'championship', 'frien
+ 'comfortable', 'diffusion', 'murder', 'policeman', 'science', 'desert', 'basketb
+ 'blood', 'funeral', 'silence', 'garment', 'merchant', 'spirit', 'punishment', 's
+ 'measurement', 'ocean', 'digital', 'illusion', 'tyrant', 'castle', 'passion', 'p
+ 'magician', 'remedy', 'knowledge', 'threshold', 'number', 'vision', 'expectation
+ 'absence', 'mystery', 'morning', 'device', 'thoughts']
+
+# Function that picks a random word from the word bank
+
+def pick_word():
+
+
+# Function that checks if inputted charcter belongs in the word
+
+def check_character():
+    
+
